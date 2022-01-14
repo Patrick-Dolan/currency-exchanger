9ac42607cd5b9873df6ea8f06612d4c049c01ca1
Patrick Dolan <dolanp1992@gmail.com>
2022-01-13 18:23:24 -0800

    add clearFields function

--- src/index.js
+++ src/index.js
@@ -4,11 +4,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import ExchangeRateService from "./js/ExchangeRateService";
 
+function clearFields() {
+  $("#amountToExchange").val("");
+  $("#currencyToExchangeTo").val("");
+  $("#conversionResults").val("");
+  $("#errors").val("");
+}
+
 $(document).ready(() => {
   $("#convertButton").submit((event) => {
     event.preventDefault();
     let amountToExchange = $("#amountToExchange").val();
     let currencyToExchangeTo = $("#currencyToExchangeTo").val();
-    
+    clearFields();
   });
 });
