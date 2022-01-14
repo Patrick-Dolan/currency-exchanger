c8d898ffd3d48c9b2aa6189b2a49e40ec5d86ce4
Patrick Dolan <dolanp1992@gmail.com>
2022-01-13 18:19:39 -0800

    add import for ExchangeRateService and declare variables for form

--- src/index.js
+++ src/index.js
@@ -2,3 +2,13 @@ import $ from "jquery";
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import ExchangeRateService from "./js/ExchangeRateService";
+
+$(document).ready(() => {
+  $("#convertButton").submit((event) => {
+    event.preventDefault();
+    let amountToExchange = $("#amountToExchange").val();
+    let currencyToExchangeTo = $("#currencyToExchangeTo").val();
+    
+  });
+});
